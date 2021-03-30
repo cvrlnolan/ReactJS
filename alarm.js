c8ef24261e54557fa06d9ec4bc639eb1ac3f7135
George Carl <42778495+hxxdini@users.noreply.github.com>
2021-03-30 17:04:02 +0100

    Update alarm.js
    Updates ..

--- alarm.js
+++ alarm.js
@@ -1,34 +1,48 @@
 import React from "react";
 import { Switch } from "antd";
-export const alarms = [
-  {
-    id: 1,
-    name: "Alarm 1",
-    togggle: <Switch />,
-  },
-  {
-    id: 2,
-    name: "Alarm 2",
-    togggle: <Switch />,
-  },
-  {
-    id: 3,
-    name: "Alarm 3",
-    togggle: <Switch />,
-  },
-  {
-    id: 4,
-    name: "Alarm 4",
-    togggle: <Switch />,
-  },
-  {
-    id: 5,
-    name: "Alarm 5",
-    togggle: <Switch />,
-  },
-  {
-    id: 6,
-    name: "Alarm 6",
-    togggle: <Switch />,
-  },
-];
+
+
+export default function DisplayAlarms(props) {
+ const {alarmState} = props;
+ const alarms = [
+    {
+      id: 1,
+      name: "Alarm 1",
+      togggle: <Switch checked={alarmState}/>,
+    },
+    {
+      id: 2,
+      name: "Alarm 2",
+      togggle: <Switch checked={alarmState}/>,
+    },
+    {
+      id: 3,
+      name: "Alarm 3",
+      togggle: <Switch checked={alarmState}/>,
+    },
+    {
+      id: 4,
+      name: "Alarm 4",
+      togggle: <Switch checked={alarmState}/>,
+    },
+    {
+      id: 5,
+      name: "Alarm 5",
+      togggle: <Switch checked={alarmState}/>,
+    },
+    {
+      id: 6,
+      name: "Alarm 6",
+      togggle: <Switch checked={alarmState}/>,
+    },
+  ];
+  
+  return (
+    <div>
+    {alarms.map(alarm => (
+     {alarm.togggle}
+    ))}
+    </div>
+  );
+  
+}
